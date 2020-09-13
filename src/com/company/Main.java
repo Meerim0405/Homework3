cfd3f350100d0f5ac0f8e7f09412910ce3dbd7bf
Meerim0405 <sakenova_meerim@mail.ru>
2020-09-13 19:15:52 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //создание дробного массива, размером 15 элементов, сразу присваиваем значения
+        double[] arr = {2.3, -3.3, 5.5, -2.6, 8.0, -3.2, 6.7, 7.7, -8.5, 9.2, 11.5, -12.5, 13.5, 14.4, 15, 0};
+        System.out.println(arr[2]);
+
+        // цикл foreach
+        for (double x: arr){
+            System.out.println(double);
+        }
+    }
+}
